288f4dc3a0e4696420ac59cde4207d7ba392cd7e
Dominik Schloesser <dsc@dosc.net>
2018-05-23 11:14:10 +0200

    luminosity with TSL2561

---
+++ lumin.h
@@ -0,0 +1,153 @@
+// lumin.h
+// Substantial parts of the code are taken from Adafruit's example
+// sensorapi.h included with the TSL2561 library by Adafruit.
+#pragma once
+
+#include "scheduler.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+
+namespace ustd {
+class Lumin {
+  public:
+    Scheduler *pSched;
+    String name;
+    uint8_t port;
+    double luminvalue;
+    ustd::sensorprocessor luminsens = ustd::sensorprocessor(4, 600, 2.0);
+    Adafruit_TSL2561_Unified *pTsl;
+    bool bActive = false;
+
+    Lumin(String name, uint8_t port) : name(name), port(port) {
+    }
+
+    ~Lumin() {
+    }
+
+    double getLumin() {
+        double val = 0.0;
+        /* Get a new sensor event */
+        sensors_event_t event;
+        pTsl->getEvent(&event);
+
+        /* Display the results (light is measured in lux) */
+        if (event.light) {
+            val = event.light;
+#ifdef USE_SERIAL_DBG
+            Serial.print(event.light);
+            Serial.println(" lux");
+#endif
+        } else {
+            /* If event.light = 0 lux the sensor is probably saturated
+               and no reliable data could be generated! */
+#ifdef USE_SERIAL_DBG
+            Serial.println("Sensor overload");
+#endif
+        }
+        return val;
+    }
+
+    // Taken from Adafruit's sensorapi.ino example
+    void displaySensorDetails(void) {
+#ifdef USE_SERIAL_DBG
+        sensor_t sensor;
+        pTsl->getSensor(&sensor);
+        Serial.println("------------------------------------");
+        Serial.print("Sensor:       ");
+        Serial.println(sensor.name);
+        Serial.print("Driver Ver:   ");
+        Serial.println(sensor.version);
+        Serial.print("Unique ID:    ");
+        Serial.println(sensor.sensor_id);
+        Serial.print("Max Value:    ");
+        Serial.print(sensor.max_value);
+        Serial.println(" lux");
+        Serial.print("Min Value:    ");
+        Serial.print(sensor.min_value);
+        Serial.println(" lux");
+        Serial.print("Resolution:   ");
+        Serial.print(sensor.resolution);
+        Serial.println(" lux");
+        Serial.println("------------------------------------");
+        Serial.println("");
+#endif
+    }
+
+    // Taken from Adafruit's sensorapi.ino example
+    void configureSensor(void) {
+        /* You can also manually set the gain or enable auto-gain support */
+        // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light
+        // to avoid sensor saturation */ tsl.setGain(TSL2561_GAIN_16X);     /*
+        // 16x gain ... use in low light to boost sensitivity */
+        pTsl->enableAutoRange(
+            true); /* Auto-gain ... switches automatically between 1x and 16x */
+
+        /* Changing the integration time gives you better sensor resolution
+         * (402ms = 16-bit data) */
+        pTsl->setIntegrationTime(
+            TSL2561_INTEGRATIONTIME_13MS); /* fast but low resolution */
+        // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium
+        // resolution and speed   */
+        // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit
+        // data but slowest conversions */
+
+#ifdef USE_SERIAL_DBG
+        /* Update these values depending on what you've set above! */
+        Serial.println("------------------------------------");
+        Serial.print("Gain:         ");
+        Serial.println("Auto");
+        Serial.print("Timing:       ");
+        Serial.println("13 ms");
+        Serial.println("------------------------------------");
+#endif
+    }
+
+    void begin(Scheduler *_pSched) {
+        pSched = _pSched;
+
+        pTsl = new Adafruit_TSL2561_Unified(port, 12345);
+
+        if (!pTsl->begin()) {
+/* There was a problem detecting the TSL2561 ... check your
+ * connections */
+#ifdef USE_SERIAL_DBG
+            Serial.print("Ooops, no TSL2561 detected ... Check your wiring or "
+                         "I2C ADDR!");
+#endif
+        } else {
+            // give a c++11 lambda as callback scheduler task registration of
+            // this.loop():
+            std::function<void()> ft = [=]() { this->loop(); };
+            pSched->add(ft, 250000);
+
+            std::function<void(String, String, String)> fnall =
+                [=](String topic, String msg, String originator) {
+                    this->subsMsg(topic, msg, originator);
+                };
+            pSched->subscribe(name + "/luminosity/#", fnall);
+            bActive = true;
+        }
+    }
+
+    void loop() {
+        if (bActive) {
+            double val = getLumin();
+            if (luminsens.filter(&val)) {
+                luminvalue = val;
+                char buf[32];
+                sprintf(buf, "%5.3f", luminvalue);
+                pSched->publish(name + "/luminosity", buf);
+            }
+        }
+    }
+
+    void subsMsg(String topic, String msg, String originator) {
+        if (topic == name + "/luminosity/get") {
+            char buf[32];
+            sprintf(buf, "%5.3f", luminvalue);
+            pSched->publish(name + "/luminosity", buf);
+        }
+    };
+};  // Lumin
+
+}  // namespace ustd

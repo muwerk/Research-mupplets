a3720d8989eaa5a2ba3ca14e83f51900b585cb91
Dominik Schlösser <dsc@dosc.net>
2019-08-13 11:34:44 +0200

    syntax fixes

--- i2c_pwm.h
+++ i2c_pwm.h
@@ -3,28 +3,29 @@
 #pragma once
 
 #include "scheduler.h"
+#include "Wire.h"
 #include <Adafruit_PWMServoDriver.h>
 
 namespace ustd {
 class I2CPWM {
   public:
+    enum Mode { GPIO, PWM, SERVO};
     Scheduler *pSched;
     int tID;
     String name;
-    uint8_t address;
-    enum Mode { GPIO, PWM, SERVO};
     Mode mode;
+    uint8_t  i2c_address;
     int frequency;
     int servoMin=150; // out of 4096
     int servoMax=600;
     int servoFrequency=60;
     int ledFrequency=1000;
 
-    Adafruit_PWMServoDriver pPwm;
+    Adafruit_PWMServoDriver *pPwm;
     bool bActive = false;
 
-    I2CPWM(String name, int mode=Mode::PWM, uint8_t i2c_address=0x40) : name(name), 
-            mode(mode), address(i2c_address) {
+    I2CPWM(String name, Mode mode=Mode::PWM, uint8_t i2c_address=0x40) : name(name), 
+            mode(mode), i2c_address(i2c_address) {
         if (mode==Mode::SERVO) {
             frequency=servoFrequency;
         } else {
@@ -40,7 +41,7 @@ class I2CPWM {
         pPwm->setPWMFreq(frequency);
     }
 
-    setServoMinMax(int minP=150, maxP=600) { // pulses out of 4096 at 60hz (frequency)
+    void setServoMinMax(int minP=150, int maxP=600) { // pulses out of 4096 at 60hz (frequency)
         if (minP<0) minP=0;
         if (maxP>4096) maxP=4096;
         servoMin=minP;
@@ -50,49 +51,44 @@ class I2CPWM {
     void begin(Scheduler *_pSched) {
         pSched = _pSched;
 
-        pPwm = new Adafruit_PWMServoDriver(i2c_address);
+        pPwm = new Adafruit_PWMServoDriver(&Wire, i2c_address);
 
-        if (!pPwm->begin()) {
-#ifdef USE_SERIAL_DBG
-            Serial.print("Problem starting PWM i2c hardware!");
-#endif
-        } else {
-            pPwm->setPWMFreq(frequency);
-            // give a c++11 lambda as callback scheduler task registration of
-            // this.loop():
-            std::function<void()> ft = [=]() { this->loop(); };
-            tID = pSched->add(ft, name, 300000);
+        pPwm->begin();
+        pPwm->setPWMFreq(frequency);
+        // give a c++11 lambda as callback scheduler task registration of
+        // this.loop():
+        std::function<void()> ft = [=]() { this->loop(); };
+        tID = pSched->add(ft, name, 300000);
 
-            std::function<void(String, String, String)> fnall =
-                [=](String topic, String msg, String originator) {
-                    this->subsMsg(topic, msg, originator);
-                };
-            pSched->subscribe(tID, name + "/i2cpwm/#", fnall);
-            bActive = true;
-        }
+        std::function<void(String, String, String)> fnall =
+            [=](String topic, String msg, String originator) {
+                this->subsMsg(topic, msg, originator);
+            };
+        pSched->subscribe(tID, name + "/i2cpwm/#", fnall);
+        bActive = true;
     }
 
     void loop() {
     }
 
-    setState(uint8_t port, bool state) {
+    void setState(uint8_t port, bool state) {
         if (mode!=Mode::SERVO) {
             if (state) { // XXX: negative logic, save state?!
-                pwm.setPWM(pin, 4096, 0);  // turns pin fully on
+                pPwm->setPWM(port, 4096, 0);  // turns pin fully on
             } else {
-                pwm.setPWM(pin, 0, 4096);  // turns pin fully off
+                pPwm->setPWM(port, 0, 4096);  // turns pin fully off
             }
         }
     }
 
-    setUnitLevel(double level) { // 0.0 ... 1.0
+    void setUnitLevel(uint8_t port, double level) { // 0.0 ... 1.0
         if (mode!=Mode::SERVO) {
             int l1=(int)(4096.0*level);
             int l2=4096-l1;
-            pwm.setPWM(pin, l1, l2);
+            pPwm->setPWM(port, l1, l2);
         } else { // Servo
-            int pulseLen=(int)((double)(servoMax-servoMin)*level))+servoMin;
-            pwm.setPWM(pin, 0, pulseLen);
+            int pulseLen=(int)((double)(servoMax-servoMin)*level)+servoMin;
+            pPwm->setPWM(port, 0, pulseLen);
         }
     }
 
@@ -137,15 +133,15 @@ class I2CPWM {
     void subsMsg(String topic, String msg, String originator) {
         String wct=name+"/i2cpwm/set/*";
         int port;
-        if (pSched->mqttmatch(topic,wct ) {
+        if (pSched->mqttmatch(topic,wct )) {
             if (topic.length() >= wct.length()) {
-                char *p=topic.c_str();
+                const char *p=topic.c_str();
                 port=atoi(&p[name.length()+strlen("/i2cpwm/")]);
-                double level=parseUnitLevel(msg)
+                double level=parseUnitLevel(msg);
                 setUnitLevel(port, level);
             }
         }
-    };
+    }
 };  // I2CPWM
 
 }  // namespace ustd

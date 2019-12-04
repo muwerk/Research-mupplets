5ebf412cdbd0f9fa7bfcbd42a449e6873b7e6788
Dominik Schlösser <dsc@dosc.net>
2019-12-04 17:00:50 +0100

    test with hardware ok

--- shift_reg_74595.h
+++ shift_reg_74595.h
@@ -4,6 +4,19 @@
 // port_clock: connection to 74HC595 Pin 11 [SHCP]
 // port_latch: connection to 74HC595 Pin 12 [STCP]
 
+//             74HC595
+//            +--v--+
+//          Q1|1  16| Vcc
+//          Q2|2  15| Q0
+//          Q3|3  14| DS    <-- Clock
+//          Q4|4  13| OEi
+//          Q5|5  12| ST_CP <-- Latch
+//          Q6|6  11| SH_CP <-- Data
+//          Q7|7  10| MRi
+//         Gnd|8   9| Q7'
+//            +-----+
+
+
 #pragma once
 
 #include "scheduler.h"
@@ -30,15 +43,14 @@ class ShiftReg {
 
     void begin(Scheduler *_pSched) {
         pSched = _pSched;
-        cur_data=0x0;
+        
         digitalWrite(port_data, HIGH);  // first writing, then setting output-mode
         digitalWrite(port_clock, HIGH); // generates safe start state
         digitalWrite(port_latch, HIGH);
         pinMode(port_data, OUTPUT);
         pinMode(port_clock, OUTPUT);
         pinMode(port_latch, OUTPUT);
-        // give a c++11 lambda as callback scheduler task registration of
-        // this.loop():
+        writeShiftReg(0);
         auto ft = [=]() { this->loop(); };
         tID = pSched->add(ft, name, 50000);
 
@@ -54,10 +66,9 @@ class ShiftReg {
             abs_data = (cur_data & (!mask)) | (data & mask);
         }
         cur_data=abs_data;
-        shiftOut(port_data, port_clock, MSBFIRST, abs_data);
         digitalWrite(port_latch, LOW); // Pulse latch to put data on output ports
+        shiftOut(port_data, port_clock, MSBFIRST, abs_data);
         digitalWrite(port_latch, HIGH);
-        digitalWrite(port_latch, LOW);
     }
 
     void set(uint8_t data) {

1121e187c39c6d8c286d7a323f2aa70ed8f8aad3
Dominik Schlösser <dsc@dosc.net>
2019-12-09 12:38:07 +0100

    redundant code removed

--- i2c_pwm.h
+++ i2c_pwm.h
@@ -55,13 +55,9 @@ class I2CPWM {
 
         pPwm->begin();
         pPwm->setPWMFreq(frequency);
-        // give a c++11 lambda as callback scheduler task registration of
-        // this.loop():
-        /* std::function<void()> */
         auto ft = [=]() { this->loop(); };
         tID = pSched->add(ft, name, 300000);
 
-        /* std::function<void(String, String, String)>*/
         auto fnall =
             [=](String topic, String msg, String originator) {
                 this->subsMsg(topic, msg, originator);
@@ -94,45 +90,6 @@ class I2CPWM {
         }
     }
 
-/*
-    double parseUnitLevel(String msg) {
-        char buff[32];
-        int l;
-        int len = msg.length();
-        double br = 0.0;
-        memset(buff, 0, 32);
-        if (len > 31)
-            l = 31;
-        else
-            l = len;
-        strncpy(buff, (const char *)msg.c_str(), l);
-
-        if ((!strcmp((const char *)buff, "on")) ||
-            (!strcmp((const char *)buff, "true"))) {
-            br = 1.0;
-        } else {
-            if ((!strcmp((const char *)buff, "off")) ||
-                (!strcmp((const char *)buff, "false"))) {
-                br = 0.0;
-            } else {
-                if ((strlen(buff) > 4) &&
-                    (!strncmp((const char *)buff, "pct ", 4))) {
-                    br = atoi((char *)(buff + 4)) / 100.0;
-                } else {
-                    if (strlen(buff) > 1 && buff[strlen(buff) - 1] == '%') {
-                        buff[strlen(buff) - 1] = 0;
-                        br = atoi((char *)buff) / 100.0;
-                    } else {
-                        br = atof((char *)buff);
-                    }
-                }
-            }
-        }
-        if (br<0.0) br=0.0;
-        if (br>1.0) br=1.0;
-        return br;
-    }
-*/
     void subsMsg(String topic, String msg, String originator) {
         String wct=name+"/i2cpwm/set/*";
         int port;

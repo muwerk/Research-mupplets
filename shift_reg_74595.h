70b2903ac7118c9f9deea96a8795d0b95baec736
Dominik Schlösser <dsc@dosc.net>
2019-12-09 16:47:21 +0100

    inline doc

--- shift_reg_74595.h
+++ shift_reg_74595.h
@@ -25,6 +25,13 @@
 namespace ustd {
 
 class ShiftReg {
+    /*! Instantiate a 74HC595 shift register mupplet.
+    *
+    * This mupplet uses three IOs to fill a 74HC595 shift register of 8 output bits with data.
+    * It can either use the hardware SPI or 3 arbitrary IOs. Through cascading shift
+    * registers, a large number of outputs can be driven.
+    */
+ 
   public:
     Scheduler *pSched;
     int tID;
@@ -37,14 +44,30 @@ class ShiftReg {
     unsigned long bitPulseTimer[8]={0,0,0,0,0,0,0,0};
     unsigned long bitPulseDelta[8]={0,0,0,0,0,0,0,0};
 
+
     ShiftReg(String name, uint8_t port_data_mosi, uint8_t port_clock_sck, uint8_t port_latch, bool useSPI=true )
         : name(name), port_data_mosi(port_data_mosi), port_clock_sck(port_clock_sck), port_latch(port_latch), useSPI(useSPI) {
+        /*! Create 74HC595 mupplet
+         *
+         * This either use hardware SPI or 3 GPIOs. Functionality is equivalent.
+         *
+         * @param name Name of this mupplet.
+         * @param port_data_mosi MOSI hardware pin for SPI mode or any GPIO for non-SPI mode.
+         * @param port_clock_sck SCK hardware pin for SPI mode or any GPIO for non-SPI mode.
+         * @param port_latch A GPIO used to latch the outputs during programming.
+         * @param useSPI true: hardware SPI mode, false: GPIO bitbang mode.
+         */    
     }
 
     ~ShiftReg() {
     }
 
-    void begin(Scheduler *_pSched) {
+    void begin(Scheduler *_pSched, unsigned long scheduleIntervalUsec=50000) {
+        /*! Start mupplet
+         *
+         * @param pSched Pointer to scheduler object
+         * @param scheduleIntervalUsec Period for pulse-timer scheduling, default 50ms
+         */
         pSched = _pSched;
         digitalWrite(port_latch, HIGH);
         pinMode(port_latch, OUTPUT);
@@ -59,7 +82,7 @@ class ShiftReg {
         }
         writeShiftReg(0);
         auto ft = [=]() { this->loop(); };
-        tID = pSched->add(ft, name, 50000);
+        tID = pSched->add(ft, name, scheduleIntervalUsec);
 
         auto fnall = [=](String topic, String msg, String originator) {
                 this->subsMsg(topic, msg, originator);
@@ -67,6 +90,7 @@ class ShiftReg {
         pSched->subscribe(tID, name + "/shiftreg/#", fnall);
     }
 
+  private:
     void writeShiftReg(uint8_t data, uint8_t mask=0xff) {
         uint8_t abs_data=data;
         if (mask!=0xff) {
@@ -81,7 +105,16 @@ class ShiftReg {
         digitalWrite(port_latch, HIGH);
     }
 
+  public:
     void set(uint8_t data, uint8_t mask=0xff) {
+        /*! Output to 74HC595
+         *
+         * Write complete byte or masked part to 74HC595 outputs and send MQTT update message.
+         * 
+         * @param data data byte for output.
+         * @param mask masks that shows which bits should be changed to content of data. 
+         * If a bit in mask is zero, then the old content of the shift register is maintained.
+         */
         writeShiftReg(data,mask);
         char buf[32];
         sprintf(buf,"%d",cur_data);
@@ -89,6 +122,13 @@ class ShiftReg {
     }
 
     void setBit(uint8_t bit, bool val) {
+        /*! Change a single bit of 74HC595
+         *
+         * Set or reset a bit in 74HC595 shift register.
+         * 
+         * @param bit bit to change.
+         * @param val true: set bit to 1, false: set bit to zero.
+         */
         uint8_t cw=1<<bit;
         if (val) {
             set(cw,cw);
@@ -98,6 +138,16 @@ class ShiftReg {
     }
 
     void pulseBit(uint8_t bit, unsigned long ms=1000) {
+        /*! Pulse a single bit of 74HC595 shift register for duration of ms milliseconds.
+         *
+         * Set a bit in 74HC595 shift register for a time perios of ms milliseconds (one-time).
+         * 
+         * @param bit bit to pulsee.
+         * @param ms high signal duration in milliseconds. Note: default timer for this mupplet 
+         * is 50ms, which therefore is the minimum resolution for a pulse. Change parameter
+         * scheduleIntervalUsec of the begin() method to increase the resolution. (Shorter 
+         * schedule intervals allow for shorter pulses.)
+         */
         if (bit>=0 and bit<8) {
             setBit(bit, true);
             bitPulseTimer[bit]=millis();
@@ -105,6 +155,7 @@ class ShiftReg {
         }
     }
 
+  private:
     void loop() {
         for (uint8_t bit=0; bit<8; bit++) {
             if (bitPulseTimer[bit]) {

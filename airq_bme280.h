c29cbd898c9443a86eadbfcbd56f96f733b5cb40
Dominik Schlösser <dsc@dosc.net>
2020-12-28 18:54:16 +0100

    Initialize sampling rates

--- airq_bme280.h
+++ airq_bme280.h
@@ -69,6 +69,12 @@ class AirQualityBme280 {
             Serial.println(errmsg);
 #endif
         } else {
+            pAirQuality->setSampling(Adafruit_BME280::MODE_FORCED,
+                                     Adafruit_BME280::SAMPLING_X1,    // temperature
+                                     Adafruit_BME280::SAMPLING_NONE,  // pressure
+                                     Adafruit_BME280::SAMPLING_X1,    // humidity
+                                     Adafruit_BME280::FILTER_OFF);
+
             bme_temp = pAirQuality->getTemperatureSensor();
             bme_pressure = pAirQuality->getPressureSensor();
             bme_humidity = pAirQuality->getHumiditySensor();

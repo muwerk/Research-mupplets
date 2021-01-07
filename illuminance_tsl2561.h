4d6211d6e793574c14770c9ce66f0ab0317513fd
Dominik Schlösser <dsc@dosc.net>
2021-01-07 13:28:40 +0100

    Vastly improved sensor sampling

--- illuminance_tsl2561.h
+++ illuminance_tsl2561.h
@@ -8,13 +8,29 @@
 #include <Adafruit_TSL2561_U.h>
 
 namespace ustd {
-class Illuminance {
+class IlluminanceTsl2561 {
+    /*! Support for TSL256 light-to-digital converter that approximates human eye response */
   public:
-    String TSL_VERSION = "0.1.0";
+    String TSL_VERSION = "0.2.0";
+    enum SampleGainMode {
+        FAST_GAINX1,
+        FAST_GAINX16,
+        MEDIUM_GAINX1,
+        MEDIUM_GAINX16,
+        PRECISE_GAINX1,
+        PRECISE_GAINX16
+    };
+    enum FilterMode { FAST, MEDIUM, LONGTERM };
     Scheduler *pSched;
     int tID;
+
     String name;
-    uint8_t port;
+    uint8_t i2c_address;
+    SampleGainMode sampleGainMode;
+    double amp = 1.0;
+
+    FilterMode filterMode;
+    unsigned long usSampleThread = 250000;  // default poll time in us.
     double luxvalue = 0.0;
     double unitIlluminanceValue = 0.0;
     double maxLux = 800.0;
@@ -22,46 +38,88 @@ class Illuminance {
 
     Adafruit_TSL2561_Unified *pTsl;
     bool bActive = false;
-    tsl2561Gain_t tGain = TSL2561_GAIN_1X;
-    tsl2561IntegrationTime_t tSpeed = TSL2561_INTEGRATIONTIME_101MS;
-    bool bAutogain = false;
-    double amp = 1.0;
+    tsl2561Gain_t tsl2561Gain = TSL2561_GAIN_1X;
+    tsl2561IntegrationTime_t tsl2561Speed = TSL2561_INTEGRATIONTIME_101MS;
 #ifdef __ESP__
     HomeAssistant *pHA;
 #endif
 
-    Illuminance(String _name, uint8_t _port, String _gain = "16x", String _speed = "fast",
-                double _amp = 1.0) {
-        name = _name;
-        port = _port;
-        amp = _amp;
-        if (_gain == "auto")
-            bAutogain = true;
-        else {
-            if (_gain = "16x") {
-                tGain = TSL2561_GAIN_16X;
-            } else if (_gain = "1x") {
-                tGain = TSL2561_GAIN_1X;
-            } else {
-#ifdef USE_SERIAL_DBG
-                Serial.println("Illegal gain mode, reverting to gain 1x");
-#endif
-            }
-        }
-        if (_speed == "fast")
-            tSpeed = TSL2561_INTEGRATIONTIME_13MS;
-        else if (_speed == "medium")
-            tSpeed = TSL2561_INTEGRATIONTIME_101MS;
-        else if (_speed == "long")
-            tSpeed = TSL2561_INTEGRATIONTIME_402MS;
-        else {
-#ifdef USE_SERIAL_DBG
-            Serial.println("Illegal speed mode, reverting to medium");
-#endif
+    IlluminanceTsl2561(String name, uint8_t i2c_address,
+                       SampleGainMode sampleGainMode = PRECISE_GAINX16, double amp = (double)1.0)
+        : name(name), i2c_address(i2c_address), sampleGainMode(sampleGainMode), amp(amp) {
+        switch (sampleGainMode) {
+        case FAST_GAINX1:
+            tsl2561Speed = TSL2561_INTEGRATIONTIME_13MS;
+            tsl2561Gain = TSL2561_GAIN_1X;
+            usSampleThread = 250000;
+            setFilterMode(FAST, true);
+            break;
+        case FAST_GAINX16:
+            tsl2561Speed = TSL2561_INTEGRATIONTIME_13MS;
+            tsl2561Gain = TSL2561_GAIN_16X;
+            usSampleThread = 250000;
+            setFilterMode(FAST, true);
+            break;
+        case MEDIUM_GAINX1:
+            tsl2561Speed = TSL2561_INTEGRATIONTIME_101MS;
+            tsl2561Gain = TSL2561_GAIN_1X;
+            usSampleThread = 500000;
+            setFilterMode(MEDIUM, true);
+            break;
+        case MEDIUM_GAINX16:
+            tsl2561Speed = TSL2561_INTEGRATIONTIME_101MS;
+            tsl2561Gain = TSL2561_GAIN_16X;
+            usSampleThread = 500000;
+            setFilterMode(MEDIUM, true);
+            break;
+        case PRECISE_GAINX1:
+            tsl2561Speed = TSL2561_INTEGRATIONTIME_402MS;
+            tsl2561Gain = TSL2561_GAIN_1X;
+            usSampleThread = 1000000;
+            setFilterMode(LONGTERM, true);
+            break;
+        case PRECISE_GAINX16:
+            tsl2561Speed = TSL2561_INTEGRATIONTIME_402MS;
+            tsl2561Gain = TSL2561_GAIN_16X;
+            usSampleThread = 1000000;
+            setFilterMode(LONGTERM, true);
+            break;
+        default:
+            DBG("Undefined state for TSL2561 SampleMode");
+            break;
         }
     }
 
-    ~Illuminance() {
+    ~IlluminanceTsl2561() {
+    }
+
+    void setFilterMode(FilterMode mode, bool silent = false) {
+        switch (mode) {
+        case FAST:
+            filterMode = FAST;
+            illuminanceSensor.smoothInterval = 1;
+            illuminanceSensor.pollTimeSec = 15;
+            illuminanceSensor.eps = 1.0;
+            illuminanceSensor.reset();
+            break;
+        case MEDIUM:
+            filterMode = MEDIUM;
+            illuminanceSensor.smoothInterval = 2;
+            illuminanceSensor.pollTimeSec = 300;
+            illuminanceSensor.eps = 1.0;
+            illuminanceSensor.reset();
+            break;
+        case LONGTERM:
+        default:
+            filterMode = LONGTERM;
+            illuminanceSensor.smoothInterval = 4;
+            illuminanceSensor.pollTimeSec = 600;
+            illuminanceSensor.eps = 0.1;
+            illuminanceSensor.reset();
+            break;
+        }
+        if (!silent)
+            publishFilterMode();
     }
 
     double calcLux() {
@@ -73,16 +131,11 @@ class Illuminance {
         /* Display the results (light is measured in lux) */
         if (event.light) {
             val = event.light;
-#ifdef USE_SERIAL_DBG
-            Serial.print(event.light);
-            Serial.println(" lux");
-#endif
         } else {
             /* If event.light = 0 lux the sensor is probably saturated
-               and no reliable data could be generated! */
-#ifdef USE_SERIAL_DBG
-            Serial.println("TSL2561 illuminance-sensor overload");
-#endif
+               and no reliable data could be generated!
+               Unfortunately this can't be discriminated from darkness! */
+            val = 0.0;
         }
         return val;
     }
@@ -123,18 +176,18 @@ class Illuminance {
 
     // Taken from Adafruit's sensorapi.ino example
     void configureSensor(void) {
-        if (bAutogain) {
-            /* Auto-gain ... switches automatically between 1x and 16x */
-            pTsl->enableAutoRange(true);
-        } else {
-            /* You can also manually set the gain or enable auto-gain support */
-            // pTsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright
-            // light to avoid sensor saturation */
-            /* // 16x gain ... use in low light to boost sensitivity */
-            pTsl->setGain(tGain);
-        }
+        // if (bAutogain) {
+        //    /* Auto-gain ... switches automatically between 1x and 16x */
+        //    pTsl->enableAutoRange(true);
+        //} else {
+        /* You can also manually set the gain or enable auto-gain support */
+        // pTsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright
+        // light to avoid sensor saturation */
+        /* // 16x gain ... use in low light to boost sensitivity */
+        pTsl->setGain(tsl2561Gain);
+        //}
 
-        pTsl->setIntegrationTime(tSpeed);
+        pTsl->setIntegrationTime(tsl2561Speed);
         /* Changing the integration time gives you better sensor resolution
          * (402ms = 16-bit data) */
         // pTsl->setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium
@@ -146,30 +199,23 @@ class Illuminance {
     void begin(Scheduler *_pSched) {
         pSched = _pSched;
 
-        pTsl = new Adafruit_TSL2561_Unified(port, 12345);
+        pTsl = new Adafruit_TSL2561_Unified(i2c_address, 12345);
 
         if (!pTsl->begin()) {
-/* There was a problem detecting the TSL2561 ... check your
- * connections */
-#ifdef USE_SERIAL_DBG
-            Serial.print("Ooops, no TSL2561 detected ... Check your wiring or "
-                         "I2C ADDR!");
-#endif
+            /* There was a problem detecting the TSL2561 ... check your
+             * connections */
+            DBG("No TSL2561 detected, check your wiring or i2c_address (usually 0x29, 0x39, or "
+                "0x49)");
         } else {
-            // give a c++11 lambda as callback scheduler task registration of
-            // this.loop():
-            /* std::function<void()> */
             auto ft = [=]() { this->loop(); };
-            tID = pSched->add(ft, name, 250000);
+            tID = pSched->add(ft, name, usSampleThread);
 
-            /* std::function<void(String, String, String)> */
             auto fnall = [=](String topic, String msg, String originator) {
                 this->subsMsg(topic, msg, originator);
             };
             pSched->subscribe(tID, name + "/sensor/illuminance/#", fnall);
             pSched->subscribe(tID, name + "/sensor/unitilluminance/#", fnall);
             bActive = true;
-            pSched->subscribe(tID, name + "/sensor/unitilluminance/#", fnall);
         }
     }
 
@@ -185,23 +231,45 @@ class Illuminance {
     }
 #endif
 
+    void publishFilterMode() {
+        switch (filterMode) {
+        case FilterMode::FAST:
+            pSched->publish(name + "/sensor/mode", "FAST");
+            break;
+        case FilterMode::MEDIUM:
+            pSched->publish(name + "/sensor/mode", "MEDIUM");
+            break;
+        case FilterMode::LONGTERM:
+            pSched->publish(name + "/sensor/mode", "LONGTERM");
+            break;
+        }
+    }
+
     void publishIlluminance() {
         char buf[32];
-        sprintf(buf, "%4.0f", luxvalue);
+        sprintf(buf, "%7.0f", luxvalue);
         pSched->publish(name + "/sensor/illuminance", buf);
-        sprintf(buf, "%5.3f", unitIlluminanceValue);
+        sprintf(buf, "%6.3f", unitIlluminanceValue);
         pSched->publish(name + "/sensor/unitilluminance", buf);
+    }
+
+    void publishMaxLux() {
+        char buf[32];
+        sprintf(buf, "%8.1f", maxLux);
+        pSched->publish(name + "/sensor/maxlux", buf);
     }
 
     void loop() {
         if (bActive) {
             double val = calcLux() * amp;
-            if (illuminanceSensor.filter(&val)) {
-                luxvalue = val;
-                unitIlluminanceValue = val / maxLux;
-                if (unitIlluminanceValue > 1.0)
-                    unitIlluminanceValue = 1.0;
-                publishIlluminance();
+            if (val != -1.0) {
+                if (illuminanceSensor.filter(&val)) {
+                    luxvalue = val;
+                    unitIlluminanceValue = val / maxLux;
+                    if (unitIlluminanceValue > 1.0)
+                        unitIlluminanceValue = 1.0;
+                    publishIlluminance();
+                }
             }
         }
     }
@@ -210,6 +278,30 @@ class Illuminance {
         if (topic == name + "/sensor/illuminance/get" ||
             topic == name + "/sensor/unitilluminance/get") {
             publishIlluminance();
+        }
+        if (topic == name + "/sensor/maxlux/set") {
+            maxLux = msg.toFloat();
+            if (maxLux < 10.0)
+                maxLux = 10.0;
+            if (maxLux > 100000.0)
+                maxLux = 100000.0;
+        }
+        if (topic == name + "/sensor/maxlux/get") {
+            publishMaxLux();
+        }
+        if (topic == name + "/sensor/mode/set") {
+            if (msg == "fast" || msg == "FAST") {
+                setFilterMode(FilterMode::FAST);
+            } else {
+                if (msg == "medium" || msg == "MEDIUM") {
+                    setFilterMode(FilterMode::MEDIUM);
+                } else {
+                    setFilterMode(FilterMode::LONGTERM);
+                }
+            }
+        }
+        if (topic == name + "/sensor/mode/get") {
+            publishFilterMode();
         }
     };
 };  // Illuminance
